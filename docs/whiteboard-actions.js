3611cc41656575042fc9398133a823e082d20c42
HAY2045 <48938217@qq.com>
2026-06-30 09:34:44 +0800

    whiteboard: load map data

--- docs/whiteboard-actions.js
+++ docs/whiteboard-actions.js
@@ -6,7 +6,7 @@ function yuanliCurrentNode(){
 
 function yuanliInfo(node){
   const map = window.YUANLI_MAP_V11 || {};
-  return map[node] || {};
+  return map[node] || {status:'draft',upstream:'',downstream:'',control:'',bottleneck:''};
 }
 
 function yuanliWriteOutput(kind){
@@ -14,9 +14,12 @@ function yuanliWriteOutput(kind){
   const info = yuanliInfo(node);
   const box = document.getElementById('whiteboard-output');
   if(!box) return;
-  if(kind === 'task'){
-    box.value = 'Task: '+node+'\n\nStatus: '+(info.status||'')+'\nControl: '+(info.control||'')+'\nBlocker: '+(info.bottleneck||'')+'\n\nOutput:\n\nAcceptance:\n\nWriteback:';
-  } else {
-    box.value = '# '+node+'\n\nStatus: '+(info.status||'')+'\nUpstream: '+(info.upstream||'')+'\nDownstream: '+(info.downstream||'')+'\nControl: '+(info.control||'')+'\nBlocker: '+(info.bottleneck||'')+'\nWriteback:';
-  }
+  const base = 'Node: '+node+'\nStatus: '+info.status+'\nUpstream: '+info.upstream+'\nDownstream: '+info.downstream+'\nControl: '+info.control+'\nBlocker: '+info.bottleneck;
+  box.value = kind === 'task' ? base+'\n\nOutput:\nAcceptance:\nWriteback:' : '# '+node+'\n\n'+base+'\nWriteback:';
 }
+
+(function(){
+  const s=document.createElement('script');
+  s.src='whiteboard-map-v1.1.js';
+  document.head.appendChild(s);
+})();
