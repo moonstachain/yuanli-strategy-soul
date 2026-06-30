6b4dc40765b0839d69ce9e051476c6806480a280
HAY2045 <48938217@qq.com>
2026-06-30 09:34:05 +0800

    whiteboard: use map v11

--- docs/whiteboard-actions.js
+++ docs/whiteboard-actions.js
@@ -4,13 +4,19 @@ function yuanliCurrentNode(){
   return checked.id.replace('n','');
 }
 
+function yuanliInfo(node){
+  const map = window.YUANLI_MAP_V11 || {};
+  return map[node] || {};
+}
+
 function yuanliWriteOutput(kind){
   const node = yuanliCurrentNode();
+  const info = yuanliInfo(node);
   const box = document.getElementById('whiteboard-output');
   if(!box) return;
   if(kind === 'task'){
-    box.value = 'Task for '+node+'\n\nInput:\n\nOutput:\n\nAcceptance:\n\nWriteback:';
+    box.value = 'Task: '+node+'\n\nStatus: '+(info.status||'')+'\nControl: '+(info.control||'')+'\nBlocker: '+(info.bottleneck||'')+'\n\nOutput:\n\nAcceptance:\n\nWriteback:';
   } else {
-    box.value = '# '+node+'\n\nQuestion:\n\nOutput:\n\nWriteback:';
+    box.value = '# '+node+'\n\nStatus: '+(info.status||'')+'\nUpstream: '+(info.upstream||'')+'\nDownstream: '+(info.downstream||'')+'\nControl: '+(info.control||'')+'\nBlocker: '+(info.bottleneck||'')+'\nWriteback:';
   }
 }
